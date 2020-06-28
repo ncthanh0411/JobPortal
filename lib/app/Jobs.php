f7bc0d5410c190162e42c26d20aed827dbdf76bb
Nguyen Cong Thanh <ncthanh0411@gmai.com>
2020-06-28 14:32:50 +0700

    Thành commit

--- lib/app/Jobs.php
+++ lib/app/Jobs.php
@@ -6,10 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Jobs extends Model
 {
-       // table name
-       protected $table = 'jobs';
-       // Primary key
-       public $primaryKey = 'id';
-       // Timestamps
-       public $timestamps = true;
+     // table name
+     protected $table = 'jobs';
+     // Primary key
+     public $primaryKey = 'id';
+     // Timestamps
+     public $timestamps = true;
 }
---
+++ lib/database/migrations/2020_06_27_151552_create_admins_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdminsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('email')->unique();
+            $table->string('password');
+
+            $table->unsignedBigInteger('manage_apply_id')->unsigned();
+            $table->foreign('manage_apply_id')
+                  ->references('id')
+                  ->on('manage_applies');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admins');
+    }
+}
