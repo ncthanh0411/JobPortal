fd0cee94fd6056713afca9449e12bd2065980fc7
lnktoan <pjpo.frs1011@gmail.com>
2020-07-05 14:37:10 +0700

    toan 5/7

--- lib/resources/views/pages/index.blade.php
+++ lib/resources/views/pages/index.blade.php
@@ -1,5 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Welcome to Index</h1>
+<link href="css/home.css" rel="stylesheet">
+    <div class="home">
+        <h3>Employers</h3>
+        <br>
+        <div class="row">
+            <div class="col-lg-3">
+                <img src="https://www.w3schools.com/howto/img_avatar.png" style="width:100%;">
+                <div class="container">
+                    <h5><b>Name</b></h5>
+                    <p style="color:red;"><span>5</span> Jobs</p>
+                </div> 
+            </div>
+
+
+            <div class="col-lg-3">
+                <img src="https://www.w3schools.com/howto/img_avatar.png" style="width:100%;">
+                <div class="container">
+                    <h5><b>Name</b></h5>
+                    <p style="color:red;"><span>5</span> Jobs</p>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <img src="https://www.w3schools.com/howto/img_avatar.png" style="width:100%;">
+                <div class="container">
+                    <h5><b>Name</b></h5>
+                    <p style="color:red;"><span>5</span> Jobs</p>
+                </div>
+            </div>
+    </div>
+    
 @endsection
