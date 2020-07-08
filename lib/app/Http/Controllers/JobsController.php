a86293acaece2886b34fcecebca4a897906ad9da
Nguyen Cong Thanh <ncthanh0411@gmai.com>
2020-07-08 22:08:27 +0700

    thanh ne

--- lib/app/Http/Controllers/JobsController.php
+++ lib/app/Http/Controllers/JobsController.php
@@ -74,11 +74,15 @@ class JobsController extends Controller
         $job_t = DB::table('jobs')->join('company_users', 'jobs.company_id', '=', 'company_users.id_com')
         ->join('categories', 'jobs.categories_id', '=', 'categories.id_cate')->where('id_job', $id)->first();
 
-        $manage = DB::table('manage_applies')->where('student_id', '=', Auth::guard('student')->user()->id_stu)->get();
-        $temp = 0;
-
-
-        return view('jobs.detail-job', compact('job_t','manage','temp'));
+        if(isset(Auth::guard('student')->user()->id_stu)) {
+            $manage = DB::table('manage_applies')->where('student_id', '=', Auth::guard('student')->user()->id_stu)->get();
+            $temp = 0;
+            return view('jobs.detail-job', compact('job_t','manage','temp'));
+        }
+        else{
+            return view('jobs.detail-job', compact('job_t'));
+        }
+        
 
         
     }
