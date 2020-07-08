354be43e0b11a3908741baee30a7946757bf31c6
Nguyen Cong Thanh <ncthanh0411@gmai.com>
2020-07-09 00:08:12 +0700

    thj

--- lib/resources/views/pages/index.blade.php
+++ lib/resources/views/pages/index.blade.php
@@ -5,7 +5,7 @@
     <div class="home">
         <h3>Employers</h3>
         <div class="row">
-            @foreach
+  
             <div class="col-lg-3">
                 <img src="https://www.w3schools.com/howto/img_avatar.png" style="width:100%;">
                 <div class="container">
@@ -13,7 +13,7 @@
                     <p style="color:red;"><span>5</span> Jobs</p>
                 </div> 
             </div>
-            @endforeach
+    
         </div>
     </div>
 @endsection
