361b767818600a1918f5b18e3627e52c58effa40
Nguyen Cong Thanh <ncthanh0411@gmai.com>
2020-07-09 00:10:55 +0700

    gvgvh

--- lib/resources/views/pages/index.blade.php
+++ lib/resources/views/pages/index.blade.php
@@ -5,17 +5,6 @@
     <div class="home">
         <h3>Employers</h3>
         <div class="row">
-<<<<<<< HEAD
-  
-            <div class="col-lg-3">
-                <img src="https://www.w3schools.com/howto/img_avatar.png" style="width:100%;">
-                <div class="container">
-                    <h5><b>Name</b></h5>
-                    <p style="color:red;"><span>5</span> Jobs</p>
-                </div> 
-            </div>
-    
-=======
             @foreach($com1 as $com2)
                 <div class="col-lg-3">
                     <a href="{{asset('student/Company/'.$com2->id_com)}}" style="text-decoration:none">
@@ -28,7 +17,6 @@
                     </a>
                 </div>
             @endforeach
->>>>>>> 68798dc11cb5608bee6fc804bfa50202e5af07d6
         </div>
         <h3>Recent Jobs</h3>
 
