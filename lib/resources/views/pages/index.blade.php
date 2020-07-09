20a97e984bd4a5f9a9d0953ea4ceaeb0ad41c7b9
lnktoan <pjpo.frs1011@gmail.com>
2020-07-10 00:01:18 +0700

    toan 10/7

--- lib/resources/views/pages/index.blade.php
+++ lib/resources/views/pages/index.blade.php
@@ -1,9 +1,105 @@
 @extends('layouts.app')
 
 @section('content')
+<style>
+    #logo {
+        width: 100%;
+    }
+
+    .info {
+        background-color: rgb(97, 199, 230);
+        color: white;
+        padding: 10px;
+        border-radius: 20px;
+        margin-bottom: 20px;
+    }
+
+     .buttons {
+    text-align: center;
+    }
+
+    .btn-hover {
+        width: 200px;
+        font-size: 16px;
+        font-weight: 600;
+        color: #fff;
+        cursor: pointer;
+        margin: 20px;
+        height: 55px;
+        text-align:center;
+        border: none;
+        background-size: 300% 100%;
+
+        border-radius: 50px;
+        /* moz-transition: all .4s ease-in-out; */
+        -o-transition: all .4s ease-in-out;
+        -webkit-transition: all .4s ease-in-out;
+        transition: all .4s ease-in-out;
+    }
+
+    .btn-hover:hover {
+        background-position: 100% 0;
+        /* moz-transition: all .4s ease-in-out; */
+        -o-transition: all .4s ease-in-out;
+        -webkit-transition: all .4s ease-in-out;
+        transition: all .4s ease-in-out;
+    }
+
+    .btn-hover:focus {
+        outline: none;
+    }
+
+    .btn-hover.color-1 {
+        background-image: linear-gradient(to right, #25aae1, #40e495, #30dd8a, #2bb673);
+        box-shadow: 0 4px 15px 0 rgba(49, 196, 190, 0.75);
+    }
+
+    .bi {
+        margin-right: 7px;
+    }
+
+    .btn {
+        text-align: right;
+    }
+
+     #description {
+        text-overflow: ellipsis;
+        overflow: hidden;
+        -webkit-line-clamp: 2;
+        -webkit-box-orient: vertical;
+        display: -webkit-box;
+    }
+    .card {
+        margin: 20px;
+        margin-left: 100px;
+        margin-right: 100px;
+        margin-bottom: 20px;
+        height:50%;
+        border: 1px solid;
+        text-decoration: none;
+        margin-top: 10px;
+        box-shadow: 0 1px 2px rgba(0,0,0,0.15);
+        transition: box-shadow 0.3s ease-in-out;
+        cursor: pointer;
+    }
+    .card:hover{
+        box-shadow: 0 5px 15px rgba(0,0,0,0.3);
+    }
+    .card-title {
+        text-overflow: ellipsis;
+        overflow: hidden;
+        -webkit-line-clamp: 1;
+        -webkit-box-orient: vertical;
+        display: -webkit-box;
+    }
+    .card-title:hover{
+        color:red;
+    }
+    
+</style>
 <link href="css/home.css" rel="stylesheet">
     <div class="home">
-        <h3>Employers</h3>
+        <h3><b>Employers</b></h3>
         <div class="row">
             @foreach($com as $com2)
                 <div class="col-lg-3">
@@ -22,7 +118,11 @@
                 </div>
             @endforeach
         </div>
-        <h3>Recent Jobs</h3>
+        <hr style="border: dashed 3px gray">
+        <a style="float: right" href="{{asset('/jobs')}}">See more</a>
+        <h3><b>Recent Jobs</b></h3>
+        
+        <br>
         @foreach($job as $job)
             @if ($job->Status == 1)    
                 <div class="card" id="list-right">
@@ -33,11 +133,10 @@
                                 <img class="card-img" src="{{asset('lib/storage/app/avatar/'.$job->logo)}}">
                             </a>
                         </div>
-
                         
-                        <div class="col-md-8">
+                        <div class="col-md-8" >
                             <div class="card-body" >
-                                <a href="{{asset('/jobs')}}/{{$job->id_job}}" id="title">
+                                <a href="{{asset('/jobs')}}/{{$job->id_job}}" id="title" style="text-decoration: none; color:black">
                                     <h4 class="card-title"><b>{{$job->title}}</b></h4>
                                     <div class="row">
                                         <div class="col-md-3">
